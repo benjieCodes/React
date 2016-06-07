179e971096105e322df47613bc21322902caa9da
Benjie Songsong <benjiesongsong@gmail.com>
2016-06-07 11:24:03 -0400

    added a line-through decoration to show which is completed

--- src/js/components/task.js
+++ src/js/components/task.js
@@ -2,10 +2,27 @@ import React from 'react';
 
 class Task extends React.Component {
 
+  constructor () {
+    super();
+    this.state = { complete: false };
+  }
+
+  toggleComplete () {
+    if (this.state.complete) {
+      this.setState({complete: false });
+    } else {
+      this.setState({complete: true });
+    }
+
+  }
+
+
   render () {
     return (
-      <li>
-        { this.props.task.name }
+      <li
+          className={ this.state.complete ? 'complete' : '' }
+          onClick={this.toggleComplete.bind(this) }>
+            { this.props.task.name }
       </li>
     );
   }
